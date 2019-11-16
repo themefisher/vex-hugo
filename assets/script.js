3a4eef548938cc17462620a9f1fe2aaf951c2d27
somrat <monnaf37@gmail.com>
2019-11-16 13:49:16 +0600

    remove extra files and make all elements more esier to use

---
+++ assets/script.js
@@ -0,0 +1,63 @@
+$(window).load(function () {
+    $("#preloader-wrapper").fadeOut("slow");
+});
+
+$(document).ready(function () {
+
+
+
+    //animated header class
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //console.log(scroll);
+        if (scroll > 200) {
+            //console.log('a');
+            $(".navigation").addClass("animated");
+        } else {
+            //console.log('a');
+            $(".navigation").removeClass("animated");
+        }
+    });
+
+
+
+    $(".gallery-slider").owlCarousel({
+        pagination: true,
+        autoPlay: 5000,
+        itemsDesktop: [1500, 4],
+        itemsDesktopSmall: [979, 3]
+    });
+
+    // Gallery Popup
+    $('.image-popup').magnificPopup({
+        type: 'image'
+    });
+
+
+
+    // animation scroll js
+    var html_body = $('html, body');
+    $('.page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 90
+                }, 1500, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // easeInOutExpo Declaration
+    jQuery.extend(jQuery.easing, {
+        easeInOutExpo: function (x, t, b, c, d) {
+            if (t == 0) return b;
+            if (t == d) return b + c;
+            if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        }
+    });
+
+});
